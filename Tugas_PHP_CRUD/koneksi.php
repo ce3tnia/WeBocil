12630d5ae45acf4f4e6f8c7880c083ca79e63af7
Cecilia Tania Emanuella <53945500+ce3tnia@users.noreply.github.com>
2019-09-29 00:45:30 +0800

    Add files via upload

---
+++ Tugas_PHP_CRUD/koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+// isi nama host, username mysql, password, nama database mysql anda
+$host = mysqli_connect("localhost","root","xxx","malasngoding");
+ 
+// isikan dengan nama database yang akan di hubungkan
+//$db = mysql_select_db("malasngoding");
+//menggunakan mysqli karena php versi 7
+?>
